14b81ee49fa6fd3ff7fbd7554b09a021b1fa775e
Tyler Haigh <tylerhaigh1994@me.com>
2018-02-24 13:09:57 +1100

    Add falling piece

---
+++ FloodControl/FallingPiece.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloodControl
+{
+    class FallingPiece : GamePiece
+    {
+        public const int FallRate = 5;
+
+        public int VerticalOffset { get; private set; }
+
+        public FallingPiece(string pieceType, int verticalOffset) : base(pieceType)
+        {
+            this.VerticalOffset = verticalOffset;
+        }
+
+        public void UpdatePiece()
+        {
+            VerticalOffset = Math.Max(0, VerticalOffset - FallRate);
+        }
+
+    }
+}
