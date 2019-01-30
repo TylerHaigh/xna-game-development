128df4c54e1ab4e6b23a32dad1284718d636ace8
Tyler Haigh <tylerhaigh1994@me.com>
2019-01-30 22:05:07 +1100

    Clean up player class

---
+++ Packt.Mono.Framework/Utilities/GameInputConverter.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Packt.Mono.Framework.Utilities
+{
+    public class KeyboardStateConverter
+    {
+        public KeyboardState State { get; set; }
+
+        public KeyboardStateConverter(KeyboardState state)
+        {
+            State = state;
+        }
+
+        public Vector2 VectorFromWASDDirections() => VectorFromWASDDirections(State);
+        public Vector2 VectorFromNumpadDirections() => VectorFromWASDDirections(State);
+
+        public static Vector2 VectorFromWASDDirections(KeyboardState state)
+        {
+            Vector2 movementVector = Vector2.Zero;
+
+            if (state.IsKeyDown(Keys.A)) movementVector.X--;
+            if (state.IsKeyDown(Keys.D)) movementVector.X++;
+
+            if (state.IsKeyDown(Keys.W)) movementVector.Y--;
+            if (state.IsKeyDown(Keys.S)) movementVector.Y++;
+
+            return movementVector;
+        }
+
+        public static Vector2 VectorFromNumpadDirections(KeyboardState state)
+        {
+            Vector2 movementVector = Vector2.Zero;
+
+            if (state.IsKeyDown(Keys.NumPad1)) movementVector += new Vector2(-1, 1);
+            if (state.IsKeyDown(Keys.NumPad2)) movementVector += new Vector2(0, 1);
+            if (state.IsKeyDown(Keys.NumPad3)) movementVector += new Vector2(1, 1);
+
+            if (state.IsKeyDown(Keys.NumPad4)) movementVector += new Vector2(-1, 0);
+            if (state.IsKeyDown(Keys.NumPad6)) movementVector += new Vector2(1, 0);
+
+            if (state.IsKeyDown(Keys.NumPad7)) movementVector += new Vector2(-1, -1);
+            if (state.IsKeyDown(Keys.NumPad8)) movementVector += new Vector2(0, -1);
+            if (state.IsKeyDown(Keys.NumPad9)) movementVector += new Vector2(1, -1);
+
+            return movementVector;
+        }
+    }
+
+
+
+    public class GamePadStateConverter
+    {
+
+        public GamePadState State { get; set; }
+
+        public GamePadStateConverter(GamePadState state)
+        {
+            State = state;
+        }
+
+        public Vector2 VectorFromLeftThumbStick() => VectorFromLeftThumbStick(State);
+        public Vector2 VectorFromRightThumbStick() => VectorFromRightThumbStick(State);
+
+        public static Vector2 VectorFromLeftThumbStick(GamePadState state)
+        {
+            return new Vector2(
+                state.ThumbSticks.Left.X,
+                -state.ThumbSticks.Left.Y
+            );
+        }
+
+        public static Vector2 VectorFromRightThumbStick(GamePadState state)
+        {
+            return new Vector2(
+                state.ThumbSticks.Right.X,
+                -state.ThumbSticks.Right.Y
+            );
+        }
+
+        
+    }
+}
--- RobotRampage/Models/Player.cs
+++ RobotRampage/Models/Player.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Packt.Mono.Framework.Entities;
 using Packt.Mono.Framework.Graphics;
+using Packt.Mono.Framework.Utilities;
 using RobotRampage.Utils;
 using System;
 using System.Collections.Generic;
@@ -15,63 +16,16 @@ namespace RobotRampage.Models
 
     class Player : GameEntity
     {
-        // TODO: See if we can attach base and turret as child entities
+        private const float PlayerSpeed = 90f;
+        private static Rectangle ScrollArea = new Rectangle(150, 100, 500, 400); // TODO: Hardcoded based on world
 
         private WorldSprite _baseSprite;
         private WorldSprite _turretSprite;
-
         private Camera _cam;
 
-        public Player(
-                Texture2D spriteTexture,
-                Rectangle baseInitialFrame,
-                int baseFrameCount,
-                Rectangle turretInitialFrame,
-                int turretFrameCount,
-                Vector2 worldLocation,
-                Camera cam)
-        {
-            BuildPlayerBase(spriteTexture, baseInitialFrame, baseFrameCount, worldLocation, cam);
-            BuildPlayerTurret(spriteTexture, turretInitialFrame, turretFrameCount, worldLocation, cam);
+        private Vector2 _moveAngle = Vector2.Zero;
 
-            Sprite = _baseSprite;
-            _cam = cam;
-
-            // TODO: Create bounding box with 4 pixel barrier
-        }
-
-
-        private void BuildPlayerBase(
-                Texture2D spriteTexture,
-                Rectangle baseInitialFrame,
-                int baseFrameCount,
-                Vector2 worldLocation,
-                Camera cam)
-        {
-
-            // Build Base sprite
-            _baseSprite = new WorldSprite(spriteTexture, baseInitialFrame, cam, worldLocation);
-
-            // Add Sprite Animation
-            _baseSprite.AddAnimation(baseInitialFrame, baseFrameCount);
-        }
-
-        private void BuildPlayerTurret(
-                Texture2D spriteTexture,
-                Rectangle turretInitialFrame,
-                int turretFrameCount,
-                Vector2 worldLocation,
-                Camera cam)
-        {
-            // Build base sprite
-            _turretSprite = new WorldSprite(spriteTexture, turretInitialFrame, cam, worldLocation);
-
-            // Add animation
-            _turretSprite.AddAnimation(turretInitialFrame, turretFrameCount);
-            _turretSprite.Animate = false;
-        }
-
-
+        public Vector2 WorldLocation { get => _baseSprite.WorldLocation; set => _baseSprite.WorldLocation = value; }
 
         public Player(WorldSprite baseSprite, WorldSprite turretSprite, Camera cam)
         {
@@ -94,7 +48,7 @@ namespace RobotRampage.Models
             // Update the player's location
             base.Update(gameTime);
 
-            _turretSprite.WorldLocation = this.Location;
+            _turretSprite.WorldLocation = this.WorldLocation;
             _turretSprite.Update(gameTime);
         }
 
@@ -107,74 +61,18 @@ namespace RobotRampage.Models
         }
 
 
-        // Handling Input
-
-        private const float PlayerSpeed = 90f;
-
-        private Vector2 HandleKeyboardMovement(KeyboardState keyState)
-        {
-
-            // This is a fairly generic method for getting movement vector.
-            // It could be refactored into a generic utility class
-            // TODO: Refactor Movement handling into dedicated class
-            Vector2 keyMovemenmt = Vector2.Zero;
-
-            if (keyState.IsKeyDown(Keys.A)) keyMovemenmt.X--;
-            if (keyState.IsKeyDown(Keys.D)) keyMovemenmt.X++;
-
-            if (keyState.IsKeyDown(Keys.W)) keyMovemenmt.Y--;
-            if (keyState.IsKeyDown(Keys.S)) keyMovemenmt.Y++;
-
-            return keyMovemenmt;
-        }
-
-        private Vector2 HandleGamePadMovement(GamePadState gamePadState)
-        {
-            return new Vector2(
-                gamePadState.ThumbSticks.Left.X,
-                -gamePadState.ThumbSticks.Left.Y
-            );
-        }
-
-        private Vector2 HandleKeyboardShots(KeyboardState keyState)
-        {
-            Vector2 shotAngle = Vector2.Zero;
-
-            if (keyState.IsKeyDown(Keys.NumPad1)) shotAngle += new Vector2(-1, 1);
-            if (keyState.IsKeyDown(Keys.NumPad2)) shotAngle += new Vector2( 0, 1);
-            if (keyState.IsKeyDown(Keys.NumPad3)) shotAngle += new Vector2( 1, 1);
-
-            if (keyState.IsKeyDown(Keys.NumPad4)) shotAngle += new Vector2(-1, 0);
-            if (keyState.IsKeyDown(Keys.NumPad6)) shotAngle += new Vector2( 1, 0);
-
-            if (keyState.IsKeyDown(Keys.NumPad7)) shotAngle += new Vector2(-1, -1);
-            if (keyState.IsKeyDown(Keys.NumPad8)) shotAngle += new Vector2( 0, -1);
-            if (keyState.IsKeyDown(Keys.NumPad9)) shotAngle += new Vector2( 1, -1);
-
-            return shotAngle;
-        }
-
-        private Vector2 HandleGamePadShots(GamePadState gamePadState)
-        {
-            return new Vector2(
-                gamePadState.ThumbSticks.Right.X,
-                -gamePadState.ThumbSticks.Right.Y
-            );
-        }
-
-        private Vector2 _moveAngle = Vector2.Zero;
+        #region InputHandling
 
         private void HandleInput(GameTime gameTime)
         {
-
             // TODO: Warning - If two devices are connected, the angles could be affected??
             Vector2 moveAngle = Vector2.Zero;
-            moveAngle += HandleKeyboardMovement(Keyboard.GetState());
-            moveAngle += HandleGamePadMovement(GamePad.GetState(PlayerIndex.One));
+            moveAngle += KeyboardStateConverter.VectorFromWASDDirections(Keyboard.GetState());
+            moveAngle += GamePadStateConverter.VectorFromLeftThumbStick(GamePad.GetState(PlayerIndex.One));
 
             Vector2 fireAngle = Vector2.Zero;
-            fireAngle += HandleKeyboardShots(Keyboard.GetState());
-            fireAngle += HandleGamePadShots(GamePad.GetState(PlayerIndex.One));
+            fireAngle += KeyboardStateConverter.VectorFromNumpadDirections(Keyboard.GetState());
+            fireAngle += GamePadStateConverter.VectorFromRightThumbStick(GamePad.GetState(PlayerIndex.One));
 
 
             if (moveAngle != Vector2.Zero)
@@ -193,27 +91,34 @@ namespace RobotRampage.Models
             }
         }
 
-        // Movement Limitation
+        #endregion
+
+
+        #region MovementLimitation
 
         private void ClampToWorld()
         {
             float x = MathHelper.Clamp(
-                _baseSprite.WorldLocation.X,
+                WorldLocation.X,
                 0,
                 _cam.WorldRectangle.Right - _baseSprite.FrameWidth
             );
 
             float y = MathHelper.Clamp(
-                _baseSprite.WorldLocation.Y,
+                WorldLocation.Y,
                 0,
                 _cam.WorldRectangle.Bottom - _baseSprite.FrameHeight
             );
 
-            _baseSprite.WorldLocation = new Vector2(x, y);
+            WorldLocation = new Vector2(x, y);
         }
 
 
-        private static Rectangle ScrollArea = new Rectangle(150, 100, 500, 400);
+        private bool PlayerMovingLeft  => _moveAngle.X < 0;
+        private bool PlayerMovingRight => _moveAngle.X > 0;
+        private bool PlayerMovingUp    => _moveAngle.Y < 0;
+        private bool PlayerMovingDown  => _moveAngle.Y > 0;
+
 
         public Vector2 GetCameraRepositionLocation(GameTime gameTime)
         {
@@ -221,20 +126,22 @@ namespace RobotRampage.Models
 
             float moveScale = PlayerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
-            if (_baseSprite.ScreenRectangle.X < ScrollArea.X && _moveAngle.X < 0)
+            if (_baseSprite.ScreenRectangle.X < ScrollArea.X && PlayerMovingLeft)
                 totalMovementOffset += new Vector2(_moveAngle.X, 0) * moveScale;
 
-            if (_baseSprite.ScreenRectangle.Right > ScrollArea.Right && _moveAngle.X > 0)
+            if (_baseSprite.ScreenRectangle.Right > ScrollArea.Right && PlayerMovingRight)
                 totalMovementOffset += new Vector2(_moveAngle.X, 0) * moveScale;
 
-            if (_baseSprite.ScreenRectangle.Y < ScrollArea.Y && _moveAngle.Y < 0)
+            if (_baseSprite.ScreenRectangle.Y < ScrollArea.Y && PlayerMovingUp)
                 totalMovementOffset += new Vector2(0, _moveAngle.Y) * moveScale;
 
-            if (_baseSprite.ScreenRectangle.Bottom > ScrollArea.Bottom && _moveAngle.Y > 0)
+            if (_baseSprite.ScreenRectangle.Bottom > ScrollArea.Bottom && PlayerMovingDown)
                 totalMovementOffset += new Vector2(0, _moveAngle.Y) * moveScale;
 
             return totalMovementOffset;
         }
+
+        #endregion
 
     }
 }
